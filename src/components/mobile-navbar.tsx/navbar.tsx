da64206e88dc126fd741020f10ea92c76a368552
adm <vkolosov1@mail.ru>
2025-08-18 16:56:35 +0300

    change trigger

--- src/components/mobile-navbar.tsx/navbar.tsx
+++ src/components/mobile-navbar.tsx/navbar.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+'use client'
+
+import React, { useState } from "react";
 import {
   Drawer,
   DrawerTrigger,
@@ -10,6 +12,8 @@ import {
 } from "../ui/shadcn/drawer";
 import Link from "next/link";
 import BrutalButton from "../ui/brutal-ui/button";
+import Button from "../ui/brutal-ui/button";
+import Image from "next/image";
 
 
 const items = [
@@ -20,12 +24,19 @@ const items = [
 
 export default function MobileNavBar() {
   return (
-    <nav className="fixed bottom-0 left-0 right-0 z-50">
+    <nav className="fixed right-5 bottom-5 z-50">
       <Drawer>
-        <DrawerTrigger className="w-full h-10 flex items-center justify-center bg-amber-400 border-4 border-black">
-          <span className="h-1 w-20 bg-black"></span>
-        </DrawerTrigger>
-
+        <DrawerTrigger asChild>
+          <Button
+            variant="outline"
+            size="lg"
+            className="w-20 h-20 rounded-none border-5 border-black  hover:text-white transition-none shadow-[12px_12px_0px_0px_#000000] hover:shadow-[0px_0px_0px_0px_#000000] hover:translate-x-2 hover:translate-y-2 font-black text-2xl transition-all divide-neutral-200"
+          >
+            <div className="flex flex-col items-center gap-1">
+              <Image src={'arrow.svg'} width={100} height={100} alt="arrow" className={`text-white ${""}`}/>
+            </div>
+            <span className="sr-only">Open navigation menu</span>
+          </Button></DrawerTrigger>
         <DrawerContent className="rounded-none border-4 border-black h-3/4 flex flex-col justify-between  ">
 
           <DrawerHeader>
